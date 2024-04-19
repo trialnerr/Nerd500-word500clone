46bc75ff6bd0ac6ef439f5c9c453a5dd8f9ca6e5
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-18 23:45:41 -0400

    linting

--- client/index.js
+++ client/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
-import { store } from "./store.js";
-import { Provider } from "react-redux";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { store } from './store.js';
+import { Provider } from 'react-redux';
 
 //import your styles
-import styles from "./scss/application.scss";
+import styles from './scss/application.scss';
 
 
-const domNode = document.getElementById("root");
+const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
   <Provider store={store}>
