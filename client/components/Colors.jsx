2b0693a5594defcdd2ee4a9f6277aaed2f5cdf76
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-19 00:14:24 -0400

    Fix linting issues and add color button accessibility

--- client/components/Colors.jsx
+++ client/components/Colors.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import * as actions from '../actions/actions';
 import { useDispatch, useSelector } from 'react-redux';
+import { current } from '@reduxjs/toolkit';
 
 
 function Colors() {
   const dispatch = useDispatch();
+   
   const currentColor = useSelector((state) => state.game.currentColor);
-
+  console.log({currentColor});
+  
   function colorChangeHandler(color) {
     dispatch(actions.changeColorActionCreator(color));
   }
@@ -14,6 +17,8 @@ function Colors() {
   function ColorButton({ color }) {
     return (
       <button
+        aria-label='color button'
+        aria-description='click to pick the current color.'
         onClick={() => colorChangeHandler(color)}
         className={`colorBtn ${color}`}
       ></button>
