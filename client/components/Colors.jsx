aa1b63322ae18d0c872a7ae58de3e1fa945aaab6
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-18 23:46:46 -0400

    deleted commented out code

--- client/components/Colors.jsx
+++ client/components/Colors.jsx
@@ -1,50 +1,7 @@
-import React from "react";
-import * as actions from "../actions/actions";
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import * as actions from '../actions/actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-// export default function Colors() {
-//   function ColorGreen() {
-//     const dispatch = useDispatch();
-//     const colorChangeHandler = () =>
-//       console.log('green was clicked');
-//       dispatch(actions.changeColorActionCreator("green"));
-//     return <button onClick={colorChangeHandler} className="colorBtn green"></button>;
-//   }
-//   function ColorYellow() {
-//     const dispatch = useDispatch();
-//     const colorChangeHandler = () =>
-//       dispatch(actions.changeColorActionCreator("yellow"));
-//     return <button onClick={colorChangeHandler} className="colorBtn yellow"></button>;
-//   }
-
-//   function ColorRed() {
-//     const dispatch = useDispatch();
-//     const colorChangeHandler = () =>
-//       dispatch(actions.changeColorActionCreator("red"));
-//     return <button onClick={colorChangeHandler} className="colorBtn red"></button>;
-//   }
-
-//   function ColorWhite() {
-//     const dispatch = useDispatch();
-//     const colorChangeHandler = () =>
-//       dispatch(actions.changeColorActionCreator("white"));
-//     return <button onClick={colorChangeHandler} className="colorBtn white"></button>;
-//   }
-
-//   return (
-//     <div className="colorsContainer">
-//       <div>
-//         <h3>ColorPicker</h3>
-//         <div className="colorsWrapper">
-//           <ColorGreen />
-//           <ColorYellow />
-//           <ColorRed />
-//           <ColorWhite />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 function Colors() {
   const dispatch = useDispatch();
