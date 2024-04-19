aa6472e5dce9e1ecc877b2a99ddeb93556bb4627
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-19 00:28:05 -0400

    Refactor Row component to use single quotes instead of double quotes and add resetRow background color

--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -1,23 +1,28 @@
-import React from "react";
-import Square from "./Square.jsx";
-import { useSelector } from "react-redux";
+import React from 'react';
+import Square from './Square.jsx';
+import { useSelector } from 'react-redux';
 
 export default function Row({ length, rowId }) {
-  
-  console.log({ rowId });
+  let backgroundColor;
+
   const currentWord = useSelector((state) => state.game.currentWord);
   const currentRow = useSelector((state) => state.game.currentRow);
   const previousWords = useSelector((state) => state.game.previousWords);
-  console.log({currentWord, currentRow, previousWords})
+  const resetRow = useSelector((state) => state.game.resetRow);
+
+  if (rowId === resetRow) backgroundColor = '#d4cfcf';
+
   const row = [];
 
   for (let i = 0; i < length; i++) {
     row.push(
       <Square
+       key = {`square${rowId}${i}`}
+      reset = {resetRow === rowId ? true : false}
         value={
           rowId === currentRow
             ? currentWord[i]
-            : (previousWords[(5 * rowId) + i] || "")
+            : previousWords[5 * rowId + i] || ''
         }
       />
     );
