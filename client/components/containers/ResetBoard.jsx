431691425e47c0d8d378b7e357d8b21489e3d619
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-25 00:01:27 -0400

    Add unique keys to ResetBoard component

--- client/components/containers/ResetBoard.jsx
+++ client/components/containers/ResetBoard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Reset from '../Reset.jsx';
 
+//update this by giving keys to each Reset component
 export default function ResetBoard() {
   
   const guessRows = [];
   for (let i = 0; i < 8; i++) {
-    guessRows.push(<Reset resetRowId = {i}/>);
+    guessRows.push(<Reset resetRowId = {i} key = {crypto.randomUUID()}/>);
   }
 
   return <div className="resetRows">{guessRows}</div>;
