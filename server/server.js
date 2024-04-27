ece97056a1082051f07fb50f32e44260d89e1daf
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-27 19:42:53 -0400

    Update comment server.js to statically serve build folder in production mode

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+// statically serve everything in the build folder on the route '/build'
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
   // serve index.html on the route '/'
@@ -9,6 +10,6 @@ if (process.env.NODE_ENV === 'production') {
     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
   });
 }
-// statically serve everything in the build folder on the route '/build'
+
 
 app.listen(3000, () => console.log('I am listening on port 3000')); //listens on port 3000 -> http://localhost:3000/
