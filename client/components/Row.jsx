817dc5763f3aae5848858184ddbdc348bc0409c9
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-27 19:50:49 -0400

    clean up square value prop

--- client/components/Row.jsx
+++ client/components/Row.jsx
@@ -3,26 +3,23 @@ import Square from './Square.jsx';
 import { useSelector } from 'react-redux';
 
 export default function Row({ length, rowId }) {
-  let backgroundColor;
 
   const currentWord = useSelector((state) => state.game.currentWord);
   const currentRow = useSelector((state) => state.game.currentRow);
   const previousWords = useSelector((state) => state.game.previousWords);
   const resetRow = useSelector((state) => state.game.resetRow);
-
-  if (rowId === resetRow) backgroundColor = '#d4cfcf';
-
   const row = [];
 
   for (let i = 0; i < length; i++) {
+
     row.push(
       <Square
        key = {`square${rowId}${i}`}
-      reset = {resetRow === rowId ? true : false}
+       reset = {resetRow === rowId}
         value={
           rowId === currentRow
-            ? currentWord[i]
-            : previousWords[5 * rowId + i] || ''
+            ? '' || currentWord[i]
+            : previousWords[5 * rowId + i] 
         }
       />
     );
