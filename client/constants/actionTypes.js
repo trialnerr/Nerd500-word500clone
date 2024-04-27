f4f39dc81b1ebf4fd32e6ba9fea94797f241a457
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-27 19:39:49 -0400

    Remove unused actions and action types

--- client/constants/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const UPDATE_LETTER = "UPDATE_LETTER";
-export const UPDATE_WORD = "UPDATE_WORD";
-export const DELETE_LETTER = "DELETE_LETTER";
-export const CHANGE_COLOR = "CHANGE_COLOR";
-export const RESET_ROW = "RESET_ROW";
-
