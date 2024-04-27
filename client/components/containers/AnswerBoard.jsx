2cef4ef1c0e0add8cde00239945b5481b5a67f2d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-27 19:51:18 -0400

    Refactor AnswerBoard component to use ColorAnswer component for rendering color squares instead redudant of Green Red Blue comps

--- client/components/containers/AnswerBoard.jsx
+++ client/components/containers/AnswerBoard.jsx
@@ -2,41 +2,32 @@ import React from 'react';
 // import Row from "../Row.jsx";
 import { useSelector } from 'react-redux';
 
+const colors = ['green', 'yellow', 'red'];
 
-function Green({id}) {
-  
-  const RGB = useSelector((state) => state.game.RGB);
-  return <div className="square green">{RGB[id] || ''}</div>;
+function ColorAnswer({ id }) {
+  const greenYellowRedCount = useSelector((state) => state.game.greenYellowRedCount);
+  for (let i = 0; i < colors.length; i++) {
+    return (
+      <div className={`square ${colors[id % 3]}`}>{greenYellowRedCount[id] || ''}</div>
+    );
+  }
 }
 
-function Yellow({id}){
-  
-  const RGB = useSelector((state) => state.game.RGB);
-  return <div className="square yellow">{RGB[id] || ''}</div>;
-}
-
-function Red({id}) {
-  const RGB = useSelector((state) => state.game.RGB);
- 
-  return <div className="square red">{RGB[id] || ''}</div>;
-}
-
-function AnswerRow({ansRowId}) {
-  return (
-    <div className="answer-container">
-      <Green id={ansRowId*3 + 0} />
-      <Yellow id={ansRowId*3 + 1} />
-      <Red id={ansRowId*3 + 2} />
-    </div>
-  );
+function AnswerRow({ ansRowId }) {
+  const colorAnswerRows = [];
+  for (let i = 0; i < 3; i++) {
+    colorAnswerRows.push(<ColorAnswer id={ansRowId * 3 + i} key = {`answerRow${i}`}/>);
+  }
+  return <div className='answer_row'>{colorAnswerRows}</div>;
 }
 
 export default function AnswerBoard() {
-  console.log('I am an answerRow', <AnswerRow />);
+  
   const answerRows = [];
   for (let i = 0; i < 8; i++) {
-    answerRows.push(<AnswerRow ansRowId={i} key={`answer_row${i}`}/>);
+    answerRows.push(
+      <AnswerRow ansRowId={i} key={`answer_row${i}`} id={`answer_row${i}`} />
+    );
   }
-
-  return <div className="answerRows">{answerRows}</div>;
+  return <div className='answerRows'>{answerRows}</div>;
 }
