31f8e908eb3b0940655f1479e7442e641c09ff54
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-28 05:27:57 -0400

    Refactor KeyBoardButton component to import keyboard functions from utilFunctions.js

--- client/components/KeyBoardButton.jsx
+++ client/components/KeyBoardButton.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
-import * as gameFunc from '../reducers/game';
+import * as gameFunc from '../utilFunctions.js/game';
+import { handleEnterKey, handleDeleteKey, handleLetterInput } from '../utilFunctions.js/keyboardFunctions.js';
 import {
   updateLetter,
   deleteLetter,
@@ -8,38 +9,30 @@ import {
 } from '../reducers/gameReducers';
 
 export default function KeyBoardButton({ alphabet }) {
-  let backgroundColor = '';
   const dispatch = useDispatch();
-
   const guess = useSelector((state) => state.game.currentWord);
   const previousWords = useSelector((state) => state.game.previousWords);
-  const wordChoice = useSelector((state) => state.game.currentWordChoice);
 
-  //update the background of the letter
-  if (previousWords.includes(alphabet)) backgroundColor = '#d4cfcf';
+  //if the letter has been used before, we want to change the background color
+  let backgroundColor = previousWords.includes(alphabet) ? '#d4cfcf' : null;
 
   //we still need to do logic for _ key
   function letterUpdateHandler() {
     if (alphabet === 'ENTER') {
       //we need to first check if the guess is longer than 5 words
-      if (guess.length === 5) {
-        const isValidWord = gameFunc.checkValid(guess);
-        const greenYellowRedCount = gameFunc.compareWithCurrentWord(
-          guess,
-          wordChoice
-        );
-        return dispatch(updateWord({ isValidWord, greenYellowRedCount }));
-      }
+      handleEnterKey(guess, dispatch, updateWord);
     } else if (alphabet === 'DEL') {
-      return dispatch(deleteLetter());
+      return handleDeleteKey(dispatch, deleteLetter);
     } else if (alphabet === 'HINT') {
       return;
     } else if (alphabet === '_') {
       return;
       //this is for other letters of the alphabet
-    } else {
+    } else if (/^[A-Z_]$/.test(alphabet)) {
       // event.target.style.backgroundColor = "grey";
-      return dispatch(updateLetter(alphabet));
+      return handleLetterInput(dispatch, updateLetter, alphabet);
+    } else {
+      return;
     }
   }
 
