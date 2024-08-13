fc4ceae888c740425468d479c3d62c751868e5ec
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-08-13 18:14:06 -0400

    Playing around with navBar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 function NavBarItem({ text }) {
-  return <div className="navBarItem">{text}</div>;
+  return <h1 className="navBarItem">{text}</h1>;
 }
 
 export default function NavBar() {
   return (
     <>
-      <div className="navBar">
-        <NavBarItem text="ModeOptions" />
-        <NavBarItem text="word500" />
-        <NavBarItem text="Stats" />
-      </div>
+      <nav className='navBar'>
+        <h1>Nerd500 - Word500 clone</h1>
+        <div>
+          <NavBarItem text='ModeOptions' />
+          <NavBarItem text='Stats' />
+        </div>
+      </nav>
       <hr />
     </>
   );
