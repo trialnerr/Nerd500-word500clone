37c3c46583a632e84deca164574bf368d31ce816
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-08-14 20:02:55 -0400

    Update NavBar component to use react-router-dom for navigation

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import {
+  Link
+} from 'react-router-dom';
 
 
 export default function NavBar() {
   return (
-    <header>
-      <nav className='navBar'>
-         <h1>Nerd500 - word500 clone</h1>
-         <div className='navLinks'>
-          <a href='/'>How to play</a>
-          <a href='/'>Contact</a>
-         </div>
-      </nav>
-    </header>
+
+      <header>
+        <nav className='navBar'>
+          <h1>Nerd500 - word500 clone</h1>
+          <div className='navLinks'>
+            <Link to='/how-to'>How To Play</Link>
+            <Link to='/contact'>Contact</Link>
+          </div>
+        </nav>
+      </header>
+  
+
   );
 }
