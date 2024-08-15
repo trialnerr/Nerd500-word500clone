dedd1110e5b3232c249e3cb00c4ce62acb7610bd
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-08-14 20:03:13 -0400

    Add HowToPlay and Contact components, and implement basic routing

--- client/index.js
+++ client/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { store } from './store.js';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import HowToPlay from './components/HowToPlay.jsx';
+import Contact from './components/Contact.jsx';
+import App from './App.jsx';
+import ErrorPage from './ErrorPage.jsx';
+
+const router = createBrowserRouter([
+    {path: '/how-to', element: <HowToPlay />},
+    {path: '/contact', element: <Contact />},
+    {path: '/', element: <App />, errorElement: <ErrorPage />}
+    
+]);
 //import your styles here
 import styles from './scss/application.scss';
-
 
 const rootNode = document.getElementById('root');
 const root = createRoot(rootNode);
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  </React.StrictMode>
 );
