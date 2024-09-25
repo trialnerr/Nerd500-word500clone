480932effe7ef5852b1eb771c42e198c3e567cc8
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-09-25 19:45:10 -0400

    Refactor App component from class to functional component

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MainContainer from './components/containers/MainContainer.jsx';
 
-class App extends Component {
-  render() {
-    return <MainContainer />;
-  }
-}
+const App = () => < MainContainer />; 
 
 export default App;
