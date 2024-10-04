6c9be0ac137369904bf6df1522c2ef62966d975c
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-04 15:54:17 -0400

    changed comment

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 // statically serve everything in the build folder on the route '/build'
 if (process.env.NODE_ENV === 'production') {
-  app.use('/build', express.static(path.join(__dirname, '../build')));
-  // serve index.html on the route '/'
+  // serve index.html on the route '*'
   app.get('*', (req, res) => {
     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
   });
+  app.use('/build', express.static(path.join(__dirname, '../build')));
 }
 
 
