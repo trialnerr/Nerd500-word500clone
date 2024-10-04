7b3bb69c0bdd2b1c1cc21a857694a43b45a5f802
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-04 12:24:55 -0400

    Refactor routing configuration in MainContainer.jsx so that navBar is present on all routes

--- client/components/containers/MainContainer.jsx
+++ client/components/containers/MainContainer.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import NavBar from '../NavBar.jsx';
 import GuessBoard from './GuessBoard.jsx';
 import AnswerBoard from './AnswerBoard.jsx';
 import ResetBoard from './ResetBoard.jsx';
 import Keyboard from '../Keyboard.jsx';
 import Colors from '../Colors.jsx';
+import HowToPlay from '../HowToPlay.jsx';
+import Contact from '../Contact.jsx';
 
 export default function MainContainer() {
 
   return (
     <div className="mainContainer">
       <NavBar />
-      <Colors />
-      <div className="boardContainer">
-        <div className="guessContainer">
-          <ResetBoard />
-          <GuessBoard />
-        </div>
-        <AnswerBoard />
-      </div>
-      <Keyboard />
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Colors />
+            <div className="boardContainer">
+              <div className="guessContainer">
+                <ResetBoard />
+                <GuessBoard />
+              </div>
+              <AnswerBoard />
+            </div>
+            <Keyboard />
+          </>
+        }
+        >
+        </Route>
+        <Route path="/how-to" element={<HowToPlay />} />
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
+      
     </div>
   );
 }
+
+
